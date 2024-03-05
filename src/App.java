e5313ad20941b2e0a814dc54b22a8c8cef6f6195
SamaraMF00 <sf1684136@gmail.com>
2024-03-05 12:11:37 -0300

    initial test

---
+++ src/App.java
@@ -0,0 +1,10 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        
+        /* Print hello */
+        System.out.println("Hello, World!");
+
+        /* Print name */
+        System.out.println("My name is Samara");
+    }
+}
